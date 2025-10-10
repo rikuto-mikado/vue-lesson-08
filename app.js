9a749e37a853199ca9f4fd658677ddbe65679098
Rikuto <mikadorikuto@gmail.com>
2025-10-10 21:38:06 +0900

    add some code

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 const app = Vue.createApp({
-    
-})
+    data() {
+        return {
+            // Boolean flags to track whether each box is selected
+            BoxASelected: false,
+            BoxBSelected: false,
+            BoxCSelected: false
+        }
+    },
+    methods: {
+        // Method to handle box selection - sets the corresponding box's selected flag to true based on the box parameter
+        boxSelected(box) {
+            if (box === 'A') {
+                this.BoxASelected = true;
+            } else if (box === 'B') {
+                this.BoxBSelected = true;
+            } else if (box === 'C') {
+                this.BoxCSelected = true;
+            }
+        }
+    }
+
+});
+
+app.mount("#styling");
